b1c341138287af77c94603f24670d2b6789c26a5
Luis Eduardo S Proencio <157709846+Luis-Eduardo-S-Proencio@users.noreply.github.com>
2024-01-28 21:54:32 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+const botao = document.querySelector(".btn-plataforma");
+const elementoPlataforma = document.querySelector(".btn-plataforma .plataformas");
+botao.addEventListener("click", () => {
+   const botaoEstaAberto = elementoPlataforma.classList.contains("ativo");
+   if(botaoEstaAberto){
+    elementoPlataforma.classList.remove("ativo");
+   } else{
+    elementoPlataforma.classList.add("ativo");
+   }
+});
